326b8aca888a9c5ec1e0332acee6af0d70828a6b
Michael Kerr <michael@vendorx.com>
2014-04-15 08:22:29 -0400

    Commit for gitlab

---
+++ influence_connections/twitter_to_con_batch.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python2.7.3
+# -*- coding: utf-8 -*-
+# Twitter to connections, batch #
+#################
+# Created Date: 2014/01/21
+# Last Updated: 2014/01/28
+
+### Resources ###
+from contextlib import closing
+import datetime
+import json
+import MySQLdb
+from pymongo import MongoClient
+import re
+import sys
+
+#import twitter_A2A as twit_con
+#import twitter_entities as twit_ent
+
+weekday_dict = {1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat', 7: 'Sun'}
+
+month_dict = {
+		'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
+		'07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+		}
+
+month_rev_dict = {
+		'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
+		'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
+		}
+
+db = MySQLdb.connect(host="ingest.cudb3djsmyrc.us-west-2.rds.amazonaws.com",
+			user="influence",
+			passwd="8RiV3wDYV6BWpKRt",
+			db="ingestdb")
+
+mongoclient = MongoClient('192.168.1.152', 27017)
+mongo_db = mongoclient['connections']
+author_collection = mongo_db['authorcons']
+
+
+### Classes ###
+
+
+### Functions ###
+def add_meta(new_dict, ):
+	## >For each list entry add the meta details
+	return new_dict
+
+
+def append_to_file(fileout, data_dict):
+	with open(fileout, 'a') as output_file:
+		output_file.write(json.dumps(data_dict) + ',\n')
+	if not output_file.closed:
+		output_file.close()
+	return
+
+
+def get_tweet_date(twitter_date_time):
+	date_time_list = twitter_date_time.split(' ')
+	tweet_date = date_time_list[5] + month_rev_dict[date_time_list[1]] + date_time_list[2]
+	tweet_time = date_time_list[3].replace(':', '')
+	return tweet_date, tweet_time
+
+
+def update_con_dict(update_dict, con_type, data_type):
+	update_dict['Connection'] = con_type
+	update_dict['Type'] = data_type
+	return update_dict
+
+
+def update_ent_dict(update_dict, ent_type, data_type):
+	update_dict['Entity'] = ent_type
+	update_dict['Type'] = data_type
+	return update_dict
+
+
+def what_day(m_d_y):
+	mdy_list = str(m_d_y).split('-')
+	return (weekday_dict[datetime.date.isoweekday(m_d_y)] + ' ' + month_dict[mdy_list[1]] + ' ' + mdy_list[2])
+
+
+def yesterday():
+	m_d_y = datetime.date.fromordinal(datetime.date.today().toordinal() - 1)
+	mdy_list = str(m_d_y).split('-')
+	return (weekday_dict[datetime.date.isoweekday(m_d_y)] + ' ' + month_dict[mdy_list[1]] + ' ' + mdy_list[2])
+
+
+### Main ###
+start_time = datetime.datetime.now()
+
+## >Get date range
+start_date = datetime.date(2014, 1, 1)
+end_date = datetime.date(2014, 3, 31)
+day = datetime.timedelta(days=1)
+daterange = lambda d1, d2: (d1 + datetime.timedelta(days=i) for i in range((d2 - d1).days + 1))
+
+## >For each day in the time period
+for tweet_date in daterange(start_date, end_date):
+	tweet_date = what_day(tweet_date)
+	query = 'SELECT * FROM raw_tweets WHERE tweet_body LIKE "%' + str(tweet_date) + '%";'
+
+	## >Twitter raw data connection
+	with closing(db.cursor()) as cur:
+		cur.execute(query)
+		tweet_data = cur.fetchall()
+
+	## >If there is any data
+	if len(tweet_data) > 0:
+		print 'Processing ' + str(len(tweet_data)) + ' tweets from ' + str(tweet_date)
+		for tweet in tweet_data:
+			data_dict = {}
+			connection_list = []
+			entity_list = []
+
+			#################################################
+			## >Discover Author Connections
+			## >Turn the tweet into dict
+			try:
+				tweet_body = json.loads(tweet[2])
+
+				## >Get the tweet id
+				data_dict['PostID'] = tweet_body['id_str']
+
+				## >Get the tweet date/time
+				data_dict['PostDate'], data_dict['PostTime'] = get_tweet_date(tweet_body['created_at'])
+
+				## >Set the network
+				data_dict['Network'] = 'twitter.com'
+
+				## >Get the tweet author
+				data_dict['Author'] = tweet_body['user']['screen_name']
+
+				## >Create the meta dict
+				data_dict['Meta'] = tweet_body['meta']
+				## >Migrate the topics to the projects
+				#TODO remove this when topics are corrected to projects
+				## >If todays date earlier than the switchover date
+				#TODO
+				if 'topics' in data_dict['Meta'].keys():
+					if 'projects' not in data_dict['Meta'].keys():
+						data_dict['Meta']['projects'] = []
+					for topic in data_dict['Meta']['topics']:
+						data_dict['Meta']['projects'].append(topic)
+					data_dict['Meta']['topics'] = []
+				## >Add the twitter id_str to the meta
+				data_dict['Meta']['id_str'] = tweet_body['user']['id_str']
+
+				## >Discover Mentions
+				if 'entities' in tweet_body.keys():
+					## >If there are any mentions
+					if len(tweet_body['entities']['user_mentions']) > 0:
+						## >Iterate through the mentions
+						for mention in tweet_body['entities']['user_mentions']:
+							## >Create the connection
+							## >Type = 'mention'
+							mention_dict = update_con_dict(data_dict, mention['screen_name'], 'Mention')
+							if mention_dict not in connection_list:
+								connection_list.append(mention_dict)
+
+				## >Discover Replies
+				if tweet_body['in_reply_to_screen_name'] is not None:
+					## >Create the connection
+					## >Type = 'reply'
+					reply_dict = update_con_dict(data_dict, tweet_body['in_reply_to_screen_name'], 'Reply')
+					if reply_dict not in connection_list:
+						connection_list.append(reply_dict)
+
+				## >Add "New" to "Active" Tweets
+				#TODO
+				if tweet_body['retweeted'] is True:
+					pass
+					#TODO add to active retweets
+
+				if tweet_body['favorited'] is True:
+					pass
+					#TODO add to active favorites
+
+				## >Get all "Active" Tweets
+				#TODO
+				## >Update retweets
+				#TODO
+				## >Update favorites
+				#TODO
+
+				## >Upload author connections to mongodb
+				for connection in connection_list:
+					if len(list(author_collection.find({
+							"PostTime": connection['PostTime'],
+							"PostDate": connection['PostDate'],
+							'PostID': connection['PostID'],
+							'Network': connection['Network'],
+							'Author': connection['Author'],
+							'Connection': connection['Connection'],
+							'Type': connection['Type']}
+							))) == 0:
+						author_collection.insert(connection)
+
+			except ValueError as e:
+				print 'JSON Exception, Logging'
+				with open('batch_log.txt', 'a') as output_file:
+					output_file.write('> ' + str(datetime.datetime.now()) + '\n')
+					output_file.write(str(e) + '\n')
+					output_file.write(str(tweet[2]) + '\n')
+
+## >Create logging details
+#TODO Add # of items added to each, and per day
+print author_collection.count()
+
+print str(datetime.datetime.now() - start_time)
---
+++ influence_connections/twitter_to_con_batch_es.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python2.7.3
+# -*- coding: utf-8 -*-
+# Twitter to connections, daily #
+#################
+# Created Date: 2014/01/21
+# Last Updated: 2014/02/28
+
+### Resources ###
+from datetime import datetime, date, time, timedelta
+import elasticsearch
+import json
+import logging
+from pymongo import MongoClient
+import sys
+import time as time_2
+
+logging.basicConfig()
+
+weekday_dict = {1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat', 7: 'Sun'}
+
+month_dict = {
+		'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
+		'07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+		}
+
+month_rev_dict = {
+		'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
+		'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
+		}
+
+#TODO read config file
+""" Elasticsearch Related information """
+es_host = 'search.vendorx.com'
+es_port = '9200'
+index_name = 'thor'
+es_timeout = 30.0
+es = elasticsearch.Elasticsearch(host=es_host, port=es_port, timeout=es_timeout)
+
+""" Mongdb related """
+mongoclient = MongoClient('192.168.1.152', 27017)
+mongo_db = mongoclient['connections']
+author_collection = mongo_db['testcons']
+
+### Classes ###
+
+
+### Functions ###
+def add_meta(new_dict, ):
+	## >For each list entry add the meta details
+	return new_dict
+
+
+def update_con_dict(update_dict, con_type, data_type):
+	update_dict['Connection'] = con_type
+	update_dict['Type'] = data_type
+	return update_dict
+
+
+def update_ent_dict(update_dict, ent_type, data_type):
+	update_dict['Entity'] = ent_type
+	update_dict['Type'] = data_type
+	return update_dict
+
+
+### Main ###
+start_time = datetime.now()
+
+# Get all "New" Tweets
+# Get the date for 'yesterday'
+start_date = date(2014, 3, 4)
+end_date = date(2014, 4, 8)
+day = timedelta(days=1)
+daterange = lambda d1, d2: (d1 + timedelta(days=i) for i in range((d2 - d1).days + 1))
+
+# For each date in the date range
+for tweet_date in daterange(start_date, end_date):
+	start_date = tweet_date.strftime('%a, %d %b %Y %H:%M:%S +0000')
+	end_date = tweet_date.strftime('%a, %d %b %Y 23:59:59 +0000')
+
+	es_query = {"from": 0,
+			"size": 1,
+			"filtered": {"filter": {"and": [
+					{"range": {"interaction.created_at": {"gte": start_date, "lte": end_date}}},
+					{"or": [
+						{"exists": {"field": "twitter.in_reply_to_screen_name"}},
+						{"exists": {"field": "twitter.mentions"}}
+						]}
+					]
+				}
+			}
+		}
+
+	# Query es
+	es_response = es.search(index=index_name, body={'query': es_query})
+	# Get number of matching tweets
+	total_tweets = es_response['hits']['total']
+
+	# If there are any new tweets
+	if total_tweets > 0:
+		#TODO LOG THIS
+		print 'Processing ' + str(total_tweets) + ' tweets from ' + str(start_date)
+
+		# Set the size to the number of tweets (Get all records)
+		es_query['size'] = total_tweets
+		es_response = es.search(index=index_name, body={'query': es_query})
+
+		# Extract the tweet data
+		tweet_data = es_response['hits']['hits']
+
+		for tweet in tweet_data:
+			data_dict = {}
+			connection_list = []
+
+			""" Author Connections """
+			# Turn the tweet into dict
+			try:
+				tweet_body = tweet['_source']['twitter']
+				interaction_content = tweet['_source']['interaction']
+
+				# Get the tweet id
+				data_dict['PostID'] = str(tweet_body['id'])
+
+				# Get the tweet date/time
+				data_dict['PostDate'] = time_2.strftime('%Y%m%d',
+									time_2.strptime(tweet_body['created_at'],
+									'%a, %d %b %Y %H:%M:%S +0000')
+								)
+
+				data_dict['PostTime'] = time_2.strftime('%H%M%S',
+									time_2.strptime(tweet_body['created_at'],
+									'%a, %d %b %Y %H:%M:%S +0000')
+								)
+
+				# Set the network
+				data_dict['Network'] = 'twitter.com'
+
+				# Get the tweet author
+				data_dict['Author'] = tweet_body['user']['screen_name'].encode('utf-8')
+
+				# Create the meta dict
+				data_dict['Meta'] = {}
+				data_dict['Meta']['usr_id_str'] = tweet_body['user']['id_str']
+
+				# Update project matching and twitter collection
+				projects = interaction_content['tag_tree']
+				data_dict['Matching'] = []
+				data_dict['Meta']['sources'] = []
+				for project, twit_source in projects.iteritems():
+					project_dict = {}
+					project_dict[u'ProjectId'] = project
+					project_dict[u'ProjectName'] = project
+					project_dict[u'Topics'] = []
+					data_dict[u'Matching'].append(project_dict)
+					data_dict['Meta']['sources'].append(twit_source)
+
+				""" Discover Mentions """
+				if 'mentions' in tweet_body.keys():
+					for mention in tweet_body['mentions']:
+						# Create the connection, Type = 'mention'
+						mention_dict = update_con_dict(data_dict, mention.encode('utf-8'), 'Mention')
+						if mention_dict not in connection_list:
+							connection_list.append(mention_dict)
+
+				""" Discover Replies """
+				if 'in_reply_to_screen_name' in tweet_body.keys():
+					# Create the connection, Type = 'reply'
+					reply_dict = update_con_dict(data_dict, tweet_body['in_reply_to_screen_name'].encode('utf-8'), 'Reply')
+					if reply_dict not in connection_list:
+						connection_list.append(reply_dict)
+
+				# Add "New" to "Active" Tweets
+				#TODO
+				#if tweet_body['retweeted'] is True:
+					#pass
+					#TODO add to active retweets
+
+				#if tweet_body['favorited'] is True:
+					#pass
+					#TODO add to active favorites
+
+				# Get all "Active" Tweets
+				#TODO
+				# Update retweets
+				#TODO
+				# Update favorites
+				#TODO
+
+				# Upload author connections to mongodb
+				for connection in connection_list:
+					if len(list(author_collection.find({
+							"PostTime": connection['PostTime'],
+							"PostDate": connection['PostDate'],
+							'PostID': connection['PostID'],
+							'Network': connection['Network'],
+							'Author': connection['Author'],
+							'Connection': connection['Connection'],
+							'Type': connection['Type']}
+							))) == 0:
+						author_collection.insert(connection)
+
+			except Exception as e:
+				print 'Exception, ' + str(e) + ' Logging'
+				with open('batch_log.txt', 'a') as output_file:
+					output_file.write('> ' + str(e) + '\n')
+					output_file.write(str(datetime.now()) + '\n')
+					output_file.write(str(e) + '\n')
+					output_file.write(str(tweet[2]) + '\n')
+
+# Create logging details
+print str(datetime.now() - start_time)
